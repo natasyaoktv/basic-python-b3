21422b3456f63637edab3aebb82087e94d43b8b3
natasyaoktv <tashyaanatashya@gmail.com>
2021-01-31 15:59:35 +0800

    Final Project

--- Final-Project/final-project.py
+++ Final-Project/final-project.py
@@ -1,16 +1,18 @@
 import smtplib, ssl
 
-
-port = 465  # For SSL
+port = 465  
 smtp_server = "smtp.gmail.com"
 
-# sender address
 sender = "ssipproject12@gmail.com" 
 
 f = open("receiver-list.txt", "r")
 x = f.readlines()
 receiver = x  
-password = input("Masukkan password dan tekan enter: ")
+
+password = ""
+while password != "Yukbisayuk123":
+    password = input("Masukkan password dan tekan enter: ")
+
 
 subject = input("Masukkan subject: ")
 content = input("Masukkan isi: ")
@@ -24,4 +26,4 @@ with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
     server.login(sender, password)
     server.sendmail(sender, receiver, message)
 
-#https://medium.com/paul-zhao-projects/sending-emails-with-python-c084b55a2857
+print("Successfully sent!")
